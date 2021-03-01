8d3d0d67328c79beeadbd7a7d29b9dc29f0944ae
zakiahsanS <79779700+zakiahsanS@users.noreply.github.com>
2021-03-01 22:27:38 +0700

    Add files via upload

---
+++ lordgans/donasi.js
@@ -0,0 +1,17 @@
+const donasi = () => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃        *_DONASI_*  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱❉ DONASI BOSS ❉⊰━━✿
+┃  
+┣━⊱ *PULSA*
+┣⊱ 0858-7633-0812
+┣━⊱ *OVO*
+┣⊱ 0856-4891-0195
+┃
+┃Gunain doank, kaga donasi
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
